28f1a8cc214dd76f57305108818fd5b633f1fa27
fynk7777 bakabonnpapa <fynk7777@yahoo.co.jp>
2024-12-20 08:30:17 +0900

    メッセージを送信する仕組みを変更

--- message.js
+++ message.js
@@ -1,5 +1,6 @@
-// 目標日時を指定 (例: 2024年12月25日 15:00)
-const targetDate = new Date(2024, 11, 25, 15, 0, 0); // 月は0から始まるので12月は11
+// 目標日時を指定 (例: 2024年12月25日 15:00)  // 月は0から始まるので12月は11
+const after = new Date(2024, 11, 24, 0, 0,0)//2024年12月24日 00:00
+const befor = new Date(2024, 11, 25, 0, 0, 0);//2024年12月25 00:00
 const message = ""
 
 // 現在の日時を取得
@@ -7,7 +8,7 @@ const currentDate = new Date();
 
 
 if (message !== ""){//メッセージが空白のときは実行しない
-  if (currentDate < targetDate) {// 現在の日付が目標日時より前かどうかを比較
+  if (after < currentDate && currentDate < befor) {// 現在の日付が目標日時より前かどうかを比較
     // 目標日時前ならアラートを表示
     alert(message);
   }
