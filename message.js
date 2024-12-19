53b6e63a9f26bf76460fbc50e289cf9aa703a684
fynk7777 bakabonnpapa <fynk7777@yahoo.co.jp>
2024-12-19 22:15:07 +0900

    再生デバイスを追加したときに設定も帰れるように変更

--- message.js
+++ message.js
@@ -1,12 +1,14 @@
 // 目標日時を指定 (例: 2024年12月25日 15:00)
 const targetDate = new Date(2024, 11, 25, 15, 0, 0); // 月は0から始まるので12月は11
-const message = "テスト中です　ID:1"
+const message = ""
 
 // 現在の日時を取得
 const currentDate = new Date();
 
 // 現在の日付が目標日時より前かどうかを比較
-if (currentDate < targetDate) {
-  // 目標日時前ならアラートを表示
-  alert(message);
+if (message !== ""){
+  if (currentDate < targetDate) {
+    // 目標日時前ならアラートを表示
+    alert(message);
+  }
 }
